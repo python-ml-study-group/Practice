c91509923de546d71f0933e31dbe92be3801baa9
pranayac <challapranaya@gmail.com>
2023-05-22 22:06:34 -0400

    added new tasks

---
+++ Pranaya/groupby_usage.py
@@ -0,0 +1,9 @@
+# Count the number of names for each first letter. 
+# Using Groupby names = ['sunaina', 'pranaya', 'srinivas', 'tarun raj', 'tarun ghanta']
+
+#import groupby from itertools
+from itertools import groupby
+names = ['sunaina', 'pranaya', 'srinivas', 'tarun raj', 'tarun ghanta']
+names.sort() #sorted('pranaya','srinivas','sunaina','tarun ghanta','tarun raj')
+g = groupby(names, lambda name:name[0] ) #grouping by first letters of a name 
+print ([(key, len(list(value))) for key, value in g])
