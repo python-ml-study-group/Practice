5ab3d8aad6eccf3c72780c492ff07d421258be12
pranayac <challapranaya@gmail.com>
2023-05-22 22:12:02 -0400

    added new tasks

--- Pranaya/groupby_usage.py
+++ Pranaya/groupby_usage.py
@@ -3,7 +3,7 @@
 
 #import groupby from itertools
 from itertools import groupby
-names = ['sunaina', 'pranaya', 'srinivas', 'tarun raj', 'tarun ghanta']
+names = ['sunaina', 'pranaya', 'srinivas', 'tarun raj', 'tarun ghanta','sridevi']
 names.sort() #sorted('pranaya','srinivas','sunaina','tarun ghanta','tarun raj')
 g = groupby(names, lambda name:name[0] ) #grouping by first letters of a name 
 print ([(key, len(list(value))) for key, value in g])
