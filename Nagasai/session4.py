52a29bf5ce9c0d28bb06c037b9a39559f36c4e5c
Paidinagasai296 <121129380+Paidinagasai296@users.noreply.github.com>
2023-05-24 20:22:26 -0300

    session4.py

--- Nagasai/session4.py
+++ Nagasai/session4.py
@@ -1,5 +1,6 @@
 import re
 from collections import defaultdict
+task1
 
 def count_word_frequency(sentences):
  word_frequency = defaultdict(int)
@@ -21,4 +22,15 @@ result = count_word_frequency(sentences)
 print(result)
 
 *************************************************
+task 2
 
+def remove_duplicates(strings):
+    unique_strings = set()
+    for string in strings:
+        sorted_string = ''.join(sorted(string.lower()))  # Sort the characters in the lowercase string
+        unique_strings.add(sorted_string)  # Add the sorted string to the set
+
+    return unique_strings
+input_list = ['abc', 'dire', 'cab', 'this', 'his']
+result = remove_duplicates(input_list)
+print(result)
