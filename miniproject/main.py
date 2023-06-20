aa6d4ecae32815b56e47abb2539641dcf408fd9c
Sridevi Thota <sr443603@dal.ca>
2023-06-19 22:31:07 -0300

    committed calculations

--- miniproject/main.py
+++ miniproject/main.py
@@ -2,8 +2,18 @@ from expense import Expense
 def input():
     pass
 
-def calcualte():
-    pass
+def calcualte(expense):
+    total_expenses = sum(expense)
+    average_expense = total_expenses / len(expense)
+    highest_expense = max(expense)
+    
+    return {
+        "total_expenses": total_expenses,
+        "average_expense": average_expense,
+        "highest_expense": highest_expense
+    }
+
+   
 
 def report ():
     pass
