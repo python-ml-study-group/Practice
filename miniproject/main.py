c5c2c2fd026d739be872a14b615f5b29e9f62acc
pranayac <challapranaya@gmail.com>
2023-06-19 21:34:24 -0400

    commited tasks

--- miniproject/main.py
+++ miniproject/main.py
@@ -1,6 +1,17 @@
 from expense import Expense
 def input_validation():
-    pass
+    while True:
+        try:
+            amount = int(input("Enter the expense amount: "))
+            category = input("Enter the expense category: ")
+            break  # Exit the loop if input is valid
+        except ValueError:
+            print("Invalid input..... Please enter a valid expense amount.")
+    
+    return amount, category
+amount, category = input_validation()
+print("Expense amount:", amount)
+print("Expense category:", category)
 
 def calcualte(expense):
     total_expenses = sum(expense)
