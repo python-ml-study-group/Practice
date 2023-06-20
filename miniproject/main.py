7ee1d55b963d94f28ad451da7ec06a4e8d320aac
Sunaina Geddam <naina23g@gmail.com>
2023-06-19 21:32:18 -0400

    added report function

--- miniproject/main.py
+++ miniproject/main.py
@@ -5,6 +5,13 @@ def input():
 def calcualte():
     pass
 
-def report ():
-    pass
+def report (expenses):
+    total_amount = sum(expenses)
+    average_expense = total_amount / len(expenses)
+    highest_expense = max(expenses)
 
+    summary_report = f"Expense Summary\n\n"
+    summary_report += f"Total amount spent: ${total_amount}\n"
+    summary_report += f"Average expense: ${average_expense:.2f}\n"
+    summary_report += f"Highest expense: ${highest_expense}\n"
+    return summary_report
